1193fc7025bc39690b16467dbce8b3883f1e0347
abhijeetsogani <soganiabhijeet@gmail.com>
2015-09-23 12:55:25 +0530

    Counters added

--- app/src/main/java/grocerylist/india/com/smscontactspoc/MainActivity.java
+++ app/src/main/java/grocerylist/india/com/smscontactspoc/MainActivity.java
@@ -11,8 +11,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /*This class fetches sms and calls
@@ -22,17 +24,26 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
     private Button call;
     private Button sms;
     private TextView text;
+    private TextView missedView;
+    private TextView incomingView;
+    private TextView outgoingView;
+    private ScrollView contactsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.display);
+        text = (TextView) findViewById(R.id.displaySms);
         call = (Button) findViewById(R.id.call);
         sms = (Button) findViewById(R.id.sms);
+        missedView = (TextView) findViewById(R.id.missedCalls);
+        incomingView = (TextView) findViewById(R.id.incomingCalls);
+        outgoingView = (TextView) findViewById(R.id.dialledCalls);
+        contactsView = (ScrollView) findViewById(R.id.scrollView);
         call.setOnClickListener(this);
         sms.setOnClickListener(this);
         text.setMovementMethod(new ScrollingMovementMethod());
+
     }
 
     @Override
@@ -74,44 +85,73 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
     }
 
     private void getCallDetails() {
+        Date lastWeekDate=getLastWeekDate();
+        //contactsView.setVisibility(View.VISIBLE);
+        text.setVisibility(View.INVISIBLE);
         StringBuffer sb = new StringBuffer();
+        StringBuffer outgoing = new StringBuffer();
+        StringBuffer incoming = new StringBuffer();
+        StringBuffer missed = new StringBuffer();
         Cursor managedCursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, null);
         int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
         int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
         int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
         int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
         sb.append("Call Log :");
+        outgoing.append("\tOUTGOING CALLS");
+        incoming.append("\tINCOMING CALLS");
+        missed.append("\tMISSED CALLS");
+
         int count = 0;
+        int missedCalls=0;
+        int incomingCalls=0;
+        int outgoingCalls=0;
         managedCursor.moveToLast();
-        while (managedCursor.moveToPrevious() && count < 10) {
+        while (managedCursor.moveToPrevious() && count < 100) {
             count++;
             String phNumber = managedCursor.getString(number);
             String callType = managedCursor.getString(type);
             String callDate = managedCursor.getString(date);
             Date callDayTime = new Date(Long.valueOf(callDate));
+            if(callDayTime.before(lastWeekDate))
+                continue;
             String callDuration = managedCursor.getString(duration);
-            String dir = null;
             int dircode = Integer.parseInt(callType);
             switch (dircode) {
                 case CallLog.Calls.OUTGOING_TYPE:
-                    dir = "OUTGOING";
+                    outgoingCalls++;
+                    outgoing.append("\nCall id: "+outgoingCalls+"\nPhone Number:--- " + phNumber + " \nCall Date:--- " + callDayTime + " \nCall duration in sec :--- " + callDuration);
+                    outgoing.append("\n----------------------------------");
                     break;
                 case CallLog.Calls.INCOMING_TYPE:
-                    dir = "INCOMING";
+                    incomingCalls++;
+                    incoming.append("\nCall id: "+incomingCalls+"\nPhone Number:--- " + phNumber + " \nCall Date:--- " + callDayTime + " \nCall duration in sec :--- " + callDuration);
+                    incoming.append("\n----------------------------------");
                     break;
                 case CallLog.Calls.MISSED_TYPE:
-                    dir = "MISSED";
+                    missedCalls++;
+                    missed.append("\nCall id: "+missedCalls+"\nPhone Number:--- " + phNumber + " \nCall Date:--- " + callDayTime + " \nCall duration in sec :--- " + callDuration);
+                    missed.append("\n----------------------------------");
                     break;
             }
-            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- " + dir + " \nCall Date:--- " + callDayTime + " \nCall duration in sec :--- " + callDuration);
-            sb.append("\n----------------------------------");
+
         }
         managedCursor.close();
-        text.setText(sb);
+        outgoing.append("TOTAL OUTGOING CALLS LAST WEEK " + outgoingCalls);
+        incoming.append("TOTAL INCOMING CALLS LAST WEEK "+incomingCalls);
+        missed.append("TOTAL MISSED CALLS LAST WEEK "+missedCalls);
+        outgoingView.setText(outgoing);
+        incomingView.setText(incoming);
+        missedView.setText(missed);
+        contactsView.setVisibility(View.VISIBLE);
+        outgoingView.setVisibility(View.VISIBLE);
+        incomingView.setVisibility(View.VISIBLE);
+        missedView.setVisibility(View.VISIBLE);
     }
 
 
     private void getSMS() {
+        contactsView.setVisibility(View.INVISIBLE);
         int count = 0;
         Uri inboxURI = Uri.parse("content://sms/inbox");
         StringBuffer sb = new StringBuffer();
@@ -132,8 +172,20 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         }
         smsInboxCursor.close();
         text.setText(sb);
+        text.setVisibility(View.VISIBLE);
 
+    }
 
+    private Date getLastWeekDate() {
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int i = c.get(Calendar.DAY_OF_WEEK) - c.getFirstDayOfWeek();
+        c.add(Calendar.DATE, -i - 7);
+        return c.getTime();
+        /*c.add(Calendar.DATE, 6);
+        Date end = c.getTime();
+        System.out.println(start + " - " + end);*/
     }
 
 }
